bf6bd9086952f37aaeea073f292bedc1380bc354
Emily Harber <emily.a.harber@gmail.com>
2019-09-10 20:58:35 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,11 +15,11 @@
 def speak_to_grandma(greeting)
   
   if greeting == "I LOVE YOU GRANDMA!"
-    puts "I LOVE YOU TOO PUMPKIN!"
+    return "I LOVE YOU TOO PUMPKIN!"
   elsif greeting != greeting.upcase  
-    puts "HUH?! SPEAK UP, SONNY!"
+    return "HUH?! SPEAK UP, SONNY!"
   else 
-    puts "NO, NOT SINCE 1938!"
+    return "NO, NOT SINCE 1938!"
   end 
   
 end  
